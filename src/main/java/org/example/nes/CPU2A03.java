6d3b301a3a3d8317ee4c0c395d071e47e508167a
Remco Abraham <remco.abraham@teamrockstars.nl>
2024-05-06 13:59:18 +0200

    Various cleanups

--- src/main/java/org/example/nes/CPU2A03.java
+++ src/main/java/org/example/nes/CPU2A03.java
@@ -149,17 +149,17 @@ public class CPU2A03 {
             case 0 -> fetchOperand0();
             case 1 -> memoryMap.get((short) toUint(op0));
             case 2 -> operandAddress = (short) toUint(memoryMap.get(getZeroPageAddress(regX)));
-            case 3 -> operandAddress = (short) (toUint(operandAddress) | (toUint(memoryMap.get((short) ((toUint(getZeroPageAddress(regX)) + 1) % 0x100))) << 8));
+            case 3 -> operandAddress = getAddressFromOperands((byte) operandAddress, memoryMap.get((short) (toUint(getZeroPageAddress((byte) (toUint(regX) + 1))))));
         }
     }
 
     private void handleAddressingIndirectYIndexed() {
         switch (getCycleInAddressing()) {
             case 0 -> fetchOperand0();
-            case 1 -> operandAddress = (short) toUint(memoryMap.get(getZeroPageAddress()));
+            case 1 -> operandAddress = (short) toUint(memoryMap.get(getZeroPageAddress((byte) 0)));
             case 2 -> {
-                operandAddress = (short) (toUint(operandAddress) | (toUint(memoryMap.get((short) ((toUint(getZeroPageAddress()) + 1) % 0x100))) << 8));
-                if (toUint(operandAddress) >>> 8 == (toUint(operandAddress) + toUint(regY)) >> 8) {
+                operandAddress = getAddressFromOperands((byte) operandAddress, memoryMap.get((short) (toUint(getZeroPageAddress((byte) 1)))));
+                if (samePage(operandAddress, (short) (toUint(operandAddress) + toUint(regY)))) {
                     cycleInInstruction++;
                 }
                 operandAddress = (short) (toUint(operandAddress) + toUint(regY));
@@ -173,7 +173,7 @@ public class CPU2A03 {
             case 0 -> fetchOperand0();
             case 1 -> {
                 fetchOperand1();
-                operandAddress = getAddressFromOperandsAndOffsetWithCarry((byte) 0);
+                operandAddress = getAddressFromOperands();
             }
             case 2 -> op0 = memoryMap.get(operandAddress);
             case 3 -> {
@@ -266,7 +266,7 @@ public class CPU2A03 {
                 op0 = memoryMap.get(operandAddress);
                 if (currentOp.addressMode == AddressMode.ACCUMULATOR) {
                     final int res = toUint(regA) << 1;
-                    applyFlags(BITMASK_ZNC, getFlagsZNC(res));
+                    applyFlagsZNC(res);
                     regA = (byte) res;
                     nextOp();
                 }
@@ -275,10 +275,14 @@ public class CPU2A03 {
             case 2 -> {
                 final int res = toUint(op0) << 1;
                 memoryMap.set(operandAddress, (byte) res);
-                applyFlags(BITMASK_ZNC, getFlagsZNC(res));
+                applyFlagsZNC(res);
                 nextOp();
             }
         }
+    }
+
+    private void applyFlagsZNC(int res) {
+        applyFlags(BITMASK_ZNC, getFlagsZNC(res));
     }
 
     private void handleBCC() {
@@ -333,7 +337,7 @@ public class CPU2A03 {
             case 2 -> {
                 final short normalNewAddress = getNextPC();
                 final short branchNewAddress = (short) (toUint(normalNewAddress) + op0);
-                memoryMap.get((short) ((toUint(branchNewAddress) % 0x100) | (getPage(normalNewAddress) << 8)));
+                memoryMap.get(getAddressFromOperands((byte) branchNewAddress, getPage(normalNewAddress)));
                 regPC = branchNewAddress;
                 resetCycleInOp();
             }
@@ -351,12 +355,12 @@ public class CPU2A03 {
     private void handleBRK() {
         switch (getCycleInOperation()) {
             case 0 -> fetchOperand0();
-            case 1 -> push((byte) ((toUint(regPC) + 2) >>> 8));
-            case 2 -> push((byte) ((toUint(regPC) + 2) & 0xff));
+            case 1 -> push(getPage((short) (toUint(regPC) + 2)));
+            case 2 -> push((byte) ((toUint(regPC) + 2)));
             case 3 -> push((byte) (toUint(regP) | BITMASK_BREAK));
             case 4 -> regPC = (short) toUint(memoryMap.get(IRQ_LO_ADDR));
             case 5 -> {
-                regPC = (short) (toUint(regPC) | (toUint(memoryMap.get(IRQ_HI_ADDR)) << 8));
+                regPC = getAddressFromOperands((byte) regPC, memoryMap.get(IRQ_HI_ADDR));
                 applyFlags(BITMASK_INT_DISABLE, BITMASK_INT_DISABLE);
                 resetCycleInOp();
             }
@@ -385,31 +389,40 @@ public class CPU2A03 {
         nextOp();
     }
 
+    private void handleCMP() {
+        handleCompare(regA);
+    }
+
+    private void handleCPX() {
+        handleCompare(regX);
+    }
+
+    private void handleCPY() {
+        handleCompare(regY);
+    }
+
+    private void handleCompare(byte reg) {
+        final int subtr = Byte.compareUnsigned(reg, memoryMap.get(operandAddress));
+        final byte flags =  (byte) (getFlagsZN((byte) subtr)
+                | (subtr >= 0 ? BITMASK_CARRY : 0));
+        applyFlags(BITMASK_ZNC, flags);
+        nextOp();
+    }
+
     private void handleDEC() {
-        switch (getCycleInOperation()) {
-            case 0 -> op0 = memoryMap.get(operandAddress);
-            case 1 -> memoryMap.set(operandAddress, op0);
-            case 2 -> {
-                final byte dec = (byte) (toUint(op0) - 1);
-                memoryMap.set(operandAddress, dec);
-                applyFlagsZN(dec);
-                nextOp();
-            }
-        }
+        handleLiteralMemoryAddition((byte) -1);
     }
 
     private void handleDEX() {
-        fetchOperand0();
-        regX = (byte) (toUint(regX) - 1);
-        applyFlagsZN(regX);
-        nextOp();
+        regX = handleDecrement(regX);
     }
 
     private void handleDEY() {
-        fetchOperand0();
-        regY = (byte) (toUint(regY) - 1);
-        applyFlagsZN(regY);
-        nextOp();
+        regY = handleDecrement(regY);
+    }
+
+    private byte handleDecrement(byte register) {
+        return handleLiteralAddition(register, (byte) -1);
     }
 
     private void handleEOR() {
@@ -420,30 +433,40 @@ public class CPU2A03 {
     }
 
     private void handleINC() {
+        handleLiteralMemoryAddition((byte) 1);
+    }
+
+    private void handleINX() {
+        regX = handleIncrement(regX);
+    }
+
+    private void handleINY() {
+        regY = handleIncrement(regY);
+    }
+
+    private byte handleIncrement(byte register) {
+        return handleLiteralAddition(register, (byte) 1);
+    }
+
+    private byte handleLiteralAddition(byte register, byte value) {
+        fetchOperand0();
+        byte newRegisterValue = (byte) (toUint(register) + value);
+        applyFlagsZN(newRegisterValue);
+        nextOp();
+        return newRegisterValue;
+    }
+
+    private void handleLiteralMemoryAddition(byte value) {
         switch (getCycleInOperation()) {
             case 0 -> op0 = memoryMap.get(operandAddress);
             case 1 -> memoryMap.set(operandAddress, op0);
             case 2 -> {
-                final byte inc = (byte) (toUint(op0) + 1);
-                memoryMap.set(operandAddress, inc);
-                applyFlagsZN(inc);
+                final byte dec = (byte) (toUint(op0) + toUint(value));
+                memoryMap.set(operandAddress, dec);
+                applyFlagsZN(dec);
                 nextOp();
             }
         }
-    }
-
-    private void handleINX() {
-        fetchOperand0();
-        regX = (byte) (toUint(regX) + 1);
-        applyFlagsZN(regX);
-        nextOp();
-    }
-
-    private void handleINY() {
-        fetchOperand0();
-        regY = (byte) (toUint(regY) + 1);
-        applyFlagsZN(regY);
-        nextOp();
     }
 
     private void handleJMP() {
@@ -490,18 +513,6 @@ public class CPU2A03 {
         return res == 0 ? BITMASK_ZERO : 0;
     }
 
-    private void handleCMP() {
-        handleCompare(regA);
-    }
-
-    private void handleCPX() {
-        handleCompare(regX);
-    }
-
-    private void handleCPY() {
-        handleCompare(regY);
-    }
-
     private short subtractPage(short address) {
         return (short) (toUint(address) - 0x100);
     }
@@ -523,25 +534,25 @@ public class CPU2A03 {
         return (short) (op0WithOffset + (toUint(op1) << 8));
     }
 
-    private short getZeroPageAddress() {
-        return getZeroPageAddress((byte) 0);
-    }
-
     private short getZeroPageAddress(byte offset) {
         return getAddressFromOperandsAndOffsetWithoutCarry(offset, op0, (byte) 0);
     }
 
     private short getAddressFromAddressAndOffsetWithoutCarry(byte offset, short address) {
-        return getAddressFromOperandsAndOffsetWithoutCarry(offset, (byte) (toUint(address) % 0x100), (byte) (toUint(address) >> 8));
+        return getAddressFromOperandsAndOffsetWithoutCarry(offset, (byte) address, getPage(address));
     }
 
     private short getAddressFromOperands() {
-        return getAddressFromOperandsAndOffsetWithoutCarry((byte) 0, op0, op1);
+        return getAddressFromOperands(op0, op1);
     }
 
     private short getAddressFromOperandsAndOffsetWithoutCarry(byte offset, byte operand0, byte operand1) {
         final int op0WithOffset = toUint(operand0) + toUint(offset);
-        return (short) ((op0WithOffset % 0x100) | (toUint(operand1) << 8));
+        return getAddressFromOperands((byte) op0WithOffset, operand1);
+    }
+
+    private short getAddressFromOperands(byte operand0, byte operand1) {
+        return (short) (toUint(operand0) | (toUint(operand1) << 8));
     }
 
     private void fetchOperand0() {
@@ -558,18 +569,6 @@ public class CPU2A03 {
 
     private int toUint(short shortValue) {
         return Short.toUnsignedInt(shortValue);
-    }
-
-    private void handleCompare(byte reg) {
-        final int subtr = Byte.compareUnsigned(reg, memoryMap.get(operandAddress));
-        final byte flags =  (byte) (getFlagsZN((byte) subtr)
-                | (subtr >= 0 ? BITMASK_CARRY : 0));
-        applyCompareFlags(flags);
-        nextOp();
-    }
-
-    private void applyCompareFlags(byte flags) {
-        applyFlags(BITMASK_ZNC, flags);
     }
 
     private void applyFlags(byte bitmask, byte flags) {
