218f3de6d0112554de4c9aadaf922fd2195a64e6
Remco Abraham <remco.abraham@teamrockstars.nl>
2024-05-09 16:05:05 +0200

    Add palette ram

--- src/main/java/org/example/nes/ppu/PPU2C02Bus.java
+++ src/main/java/org/example/nes/ppu/PPU2C02Bus.java
@@ -4,13 +4,22 @@ import org.example.nes.Bus;
 
 public class PPU2C02Bus implements Bus {
 
+    private final byte[] paletteRam = new byte[32];
+
     @Override
     public byte read(short address) {
+        final int addr = Short.toUnsignedInt(address);
+        if (addr >= 0x3F00 && addr <= 0x3FFF) {
+            return paletteRam[addr % 32];
+        }
         return 0;
     }
 
     @Override
     public void write(short address, byte value) {
-
+        final int addr = Short.toUnsignedInt(address);
+        if (addr >= 0x3F00 && addr <= 0x3FFF) {
+            paletteRam[addr % 32] = value;
+        }
     }
 }
