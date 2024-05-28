54c25160dc2ae28f6a45b5e2bfb50ef4f3457897
Remco Abraham <remco.abraham@teamrockstars.nl>
2024-05-28 14:23:47 +0200

    Clean up sprite evaluator

--- src/main/java/org/example/nes/ppu/SpriteEvaluator.java
+++ src/main/java/org/example/nes/ppu/SpriteEvaluator.java
@@ -3,8 +3,18 @@ package org.example.nes.ppu;
 import static org.example.nes.UInt.toUint;
 
 class SpriteEvaluator {
+    private static final int EVALUATION_START_CYCLE = 64;
+    private static final int LINE_LENGTH = 256;
+    private static final int LINES = 240;
+    private static final int MAX_SPRITES_IN_OAM = 64;
+    private static final int MAX_SPRITES_IN_LINE = 8;
+    private static final int SPRITE_SIZE = 4;
+    private static final int SECONDARY_OAM_SIZE = 32;
+    private static final int SPRITE_HEIGHT = 8;
+    private static final int NO_SPRITE_FOUND_ADDRESS = -1;
+
     // y, tile, attribute, x
-    private final byte[] secondaryOam = new byte[32];
+    private final byte[] secondaryOam = new byte[SECONDARY_OAM_SIZE];
     private final OAM oam;
 
     private byte oamReadBuf;
@@ -24,52 +34,141 @@ class SpriteEvaluator {
     // TODO overflow when oam addr does not start at 0
     // TODO Sprite overflow flag
     void tick() {
-        if (cycle % 2 == 0) {
-            if (cycle % 256 == 0) {
-                foundSpriteStartAddr = -1;
-                spritesEvaluated = -1;
-                spritesFound = 0;
-                secondaryOamCounter = 0;
-            }
-            oam.setPullReadsHigh(cycle % 256 < 64);
-            incrementSecondaryOamIndex = true;
-            oamReadBuf = oam.readRegOamData();
-            if (cycle % 256 >= 64) {
-                byte oamAddress = oam.readRegOamAddr();
-                final int offsetFromFoundSprite = toUint(oamAddress) - toUint(foundSpriteStartAddr);
-                if (offsetFromFoundSprite > 0 && offsetFromFoundSprite < 4 && foundSpriteStartAddr != -1) {
-                    oam.writeRegOamAddr((byte) (oamAddress + 1));
-                } else {
-                    spritesEvaluated++;
-                    if (yInRange(cycle)) {
-                        assert toUint(oamAddress) % 4 == 0;
-                        foundSpriteStartAddr = oamAddress;
-                        if (spritesFound < 8 && spritesEvaluated < 64) {
-                            spritesFound++;
-                        }
-                        oam.writeRegOamAddr((byte) (oamAddress + 1));
-                    } else {
-                        oam.writeRegOamAddr((byte) (oamAddress + 4));
-                        incrementSecondaryOamIndex = false;
-                    }
-                }
-            }
+        if (isReadCycle()) {
+            handleRead();
         } else {
-            if (secondaryOamCounter < 64 && spritesEvaluated < 64) {
-                secondaryOam[secondaryOamCounter % 32] = oamReadBuf;
-            }
-            if (incrementSecondaryOamIndex) {
-                secondaryOamCounter++;
-                incrementSecondaryOamIndex = false;
-            }
+            handleWrite();
         }
-        cycle = (cycle + 1) % (256 * 240);
+        incrementCycleCounter();
     }
 
-    private boolean yInRange(int cycle) {
+    private boolean isReadCycle() {
+        return cycle % 2 == 0;
+    }
+
+    private void handleRead() {
+        if (isFirstCycleInClear()) {
+            initializeLineState();
+            prepareOamForClear();
+        } else if (isFirstCycleInEvaluation()) {
+            prepareOamForEvaluation();
+        }
+
+        readOam();
+
+        if (isEvaluationCycle()) {
+            handleEvaluation();
+        }
+    }
+
+    private boolean isFirstCycleInClear() {
+        return getCycleInLine() == 0;
+    }
+
+    private int getCycleInLine() {
+        return cycle % LINE_LENGTH;
+    }
+
+    private void initializeLineState() {
+        foundSpriteStartAddr = NO_SPRITE_FOUND_ADDRESS;
+        spritesEvaluated = -1;
+        spritesFound = 0;
+        secondaryOamCounter = 0;
+        enableSecondaryOamAddressIncrements();
+    }
+
+    private void enableSecondaryOamAddressIncrements() {
+        incrementSecondaryOamIndex = true;
+    }
+
+    private void prepareOamForClear() {
+        oam.setPullReadsHigh(true);
+    }
+
+    private boolean isFirstCycleInEvaluation() {
+        return getCycleInLine() == EVALUATION_START_CYCLE;
+    }
+
+    private void prepareOamForEvaluation() {
+        oam.setPullReadsHigh(false);
+    }
+
+    private void readOam() {
+        oamReadBuf = oam.readRegOamData();
+    }
+
+    private boolean isEvaluationCycle() {
+        return getCycleInLine() >= EVALUATION_START_CYCLE;
+    }
+
+    private void handleEvaluation() {
+        enableSecondaryOamAddressIncrements();
+        if (isFetchingRemainingSpriteData()) {
+            setOamAddressToNextByte();
+        } else {
+            evaluateNextSprite();
+        }
+    }
+
+    private boolean isFetchingRemainingSpriteData() {
+        byte oamAddress = oam.readRegOamAddr();
+        final int offsetFromFoundSprite = toUint(oamAddress) - toUint(foundSpriteStartAddr);
+        return offsetFromFoundSprite > 0
+                && offsetFromFoundSprite < SPRITE_SIZE
+                && foundSpriteStartAddr != NO_SPRITE_FOUND_ADDRESS;
+    }
+
+    private void evaluateNextSprite() {
+        spritesEvaluated++;
+        if (spriteInRange()) {
+            foundSpriteStartAddr = oam.readRegOamAddr();
+            if (spritesFound < MAX_SPRITES_IN_LINE && spritesEvaluated < MAX_SPRITES_IN_OAM) {
+                spritesFound++;
+            }
+            setOamAddressToNextByte();
+        } else {
+            setOamAddressToNextSprite();
+            disableSecondaryOamAddressIncrements();
+        }
+    }
+
+    private boolean spriteInRange() {
         final int y = toUint(oamReadBuf);
-        final int line = cycle / 256;
-        return line >= y && line < y + 8;
+        final int line = cycle / LINE_LENGTH;
+        return line >= y && line < y + SPRITE_HEIGHT;
+    }
+
+    private void setOamAddressToNextByte() {
+        incrementOamAddress(1);
+    }
+
+    private void setOamAddressToNextSprite() {
+        incrementOamAddress(4);
+    }
+
+    private void incrementOamAddress(int inc) {
+        oam.writeRegOamAddr((byte) (oam.readRegOamAddr() + inc));
+    }
+
+    private void disableSecondaryOamAddressIncrements() {
+        incrementSecondaryOamIndex = false;
+    }
+
+    private void handleWrite() {
+        if (writesEnabled()) {
+            secondaryOam[secondaryOamCounter % SECONDARY_OAM_SIZE] = oamReadBuf;
+        }
+        if (incrementSecondaryOamIndex) {
+            secondaryOamCounter++;
+        }
+    }
+
+    private boolean writesEnabled() {
+        return secondaryOamCounter < 2 * SECONDARY_OAM_SIZE && spritesEvaluated < MAX_SPRITES_IN_OAM;
+    }
+
+    private void incrementCycleCounter() {
+        cycle = (cycle + 1) % (LINES * LINE_LENGTH);
     }
 
     byte readSecondaryOam(int index) {
