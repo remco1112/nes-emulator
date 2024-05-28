7772d16f3dbf500e80c971313e4680c47e34804b
Remco Abraham <remco.abraham@teamrockstars.nl>
2024-05-28 14:37:38 +0200

    Clean up sprite evaluator test

--- src/test/java/org/example/nes/ppu/SpriteEvaluatorTest.java
+++ src/test/java/org/example/nes/ppu/SpriteEvaluatorTest.java
@@ -12,53 +12,86 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(JUnitQuickcheck.class)
 public class SpriteEvaluatorTest {
+    private static final int OAM_SIZE = 256;
+    private byte[] oamData;
+    private OAM oam;
+    private SpriteEvaluator spriteEvaluator;
 
     @Property
-    public void secondaryOamContentsCorrectForEachLine(@Size(min = 256, max = 256) ArrayList<Byte> oamList) {
-        final Byte[] boxedOam = oamList.toArray(new Byte[256]);
-        final byte[] oamdata = new byte[256];
-        for (int i = 0; i < 256; i++) {
-            oamdata[i] = boxedOam[i];
-        }
+    public void secondaryOamContentsCorrectForEachLine(@Size(min = OAM_SIZE, max = OAM_SIZE) ArrayList<Byte> oamList) {
+        initializeFields(oamList);
 
-        final OAM oam = new OAM(oamdata);
-
-        SpriteEvaluator spriteEvaluator = new SpriteEvaluator(oam);
-        int spriteCount;
-        boolean spriteOverflow;
         for (int line = 0; line < 240; line++) {
-            spriteCount = 0;
-            spriteOverflow = false;
-
-            oam.writeRegOamAddr((byte) 0);
-
-            for (int i = 0; i < 256; i++) {
-                spriteEvaluator.tick();
-            }
-
-            for(int i = 0; i < 256; i += 4) {
-               if (line >= toUint(oamdata[i]) && line < toUint(oamdata[i]) + 8) {
-                   if (spriteCount < 8) {
-                       for (int j = 0; j < 4; j++) {
-                           assertEquals(oamdata[i + j], spriteEvaluator.readSecondaryOam(4 * spriteCount + j));
-                       }
-                       spriteCount++;
-                   } else {
-                       spriteOverflow = true;
-                       break;
-                   }
-               }
-            }
-
-            if (spriteCount < 8) {
-                if (spriteCount > 0 && spriteEvaluator.readSecondaryOam(4 * (spriteCount - 1)) != oamdata[63 * 4]) {
-                    assertEquals(oamdata[63 * 4], spriteEvaluator.readSecondaryOam(4 * spriteCount));
-                }
-
-                for (int i = 4 * spriteCount + 1; i < 32; i++) {
-                    assertEquals(-1, spriteEvaluator.readSecondaryOam(i));
-                }
-            }
+            testSecondaryOamContentsCorrectForLine(line);
         }
+    }
+
+    private void initializeFields(ArrayList<Byte> oamList) {
+        oamData = toByteArray(oamList);
+        oam = new OAM(oamData);
+        spriteEvaluator = new SpriteEvaluator(oam);
+    }
+
+    private void testSecondaryOamContentsCorrectForLine(int line) {
+        oam.writeRegOamAddr((byte) 0);
+
+        runSpriteEvaluatorForOneLine();
+
+        int spriteCount = 0;
+        for(int i = 0; i < OAM_SIZE; i += 4) {
+           if (spriteInRange(line, oamData[i])) {
+               if (spriteCount < 8) {
+                   assertSecondaryOamContainsSprite(i, 4 * spriteCount++);
+               } else {
+                   // TODO Sprite overflow
+                   break;
+               }
+           }
+        }
+
+        if (spriteCount < 8) {
+            assertYOfSprite63InSecondaryOam(spriteCount);
+            assertRemainderOfSecondaryOamClear(spriteCount);
+        }
+    }
+
+    private void assertYOfSprite63InSecondaryOam(int spriteCount) {
+        if (sprite63NotLastSpriteInSecondaryOam(spriteCount)) {
+            assertEquals(oamData[63 * 4], spriteEvaluator.readSecondaryOam(4 * spriteCount));
+        }
+    }
+
+    private boolean sprite63NotLastSpriteInSecondaryOam(int spriteCount) {
+        return spriteCount > 0 && spriteEvaluator.readSecondaryOam(4 * (spriteCount - 1)) != oamData[63 * 4];
+    }
+
+    private void assertRemainderOfSecondaryOamClear(int spriteCount) {
+        for (int i = 4 * spriteCount + 1; i < 32; i++) {
+            assertEquals(-1, spriteEvaluator.readSecondaryOam(i));
+        }
+    }
+
+    private void assertSecondaryOamContainsSprite(int spriteIndexInOam, int spriteIndexInSecondaryOam) {
+        for (int j = 0; j < 4; j++) {
+            assertEquals(oamData[spriteIndexInOam + j], spriteEvaluator.readSecondaryOam(spriteIndexInSecondaryOam + j));
+        }
+    }
+
+    private boolean spriteInRange(int line, byte spriteY) {
+        return line >= toUint(spriteY) && line < toUint(spriteY) + 8;
+    }
+
+    private void runSpriteEvaluatorForOneLine() {
+        for (int i = 0; i < OAM_SIZE; i++) {
+            spriteEvaluator.tick();
+        }
+    }
+
+    private static byte[] toByteArray(ArrayList<Byte> oamList) {
+        final byte[] oamData = new byte[OAM_SIZE];
+        for (int i = 0; i < OAM_SIZE; i++) {
+            oamData[i] = oamList.get(i);
+        }
+        return oamData;
     }
 }
